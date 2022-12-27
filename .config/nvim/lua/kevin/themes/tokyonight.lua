d571810821daa83252fd6c264afb4511208dde8f
ikevinws <kevinwang1036@gmail.com>
2022-12-27 12:08:49 -0500

    updates

--- .config/nvim/lua/kevin/themes/tokyonight.lua
+++ .config/nvim/lua/kevin/themes/tokyonight.lua
@@ -1,21 +1,14 @@
-local colorscheme_cmd = 'colorscheme tokyonight-night'
-local success, err = pcall(vim.cmd, colorscheme_cmd)
-if not success then
-    vim.api.nvim_err_writeln(err)
-end
-
 require('tokyonight').setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
     style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-    light_style = 'day', -- The theme is used when the background is set to light
     transparent = false, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
+        comments = { italic = false },
+        keywords = { italic = false },
         functions = {},
         variables = {},
         -- Background styles. Can be "dark", "transparent" or "normal"
@@ -28,3 +21,9 @@ require('tokyonight').setup({
     dim_inactive = false, -- dims inactive windows
     lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 })
+
+local colorscheme_cmd = 'colorscheme tokyonight-night'
+local success, err = pcall(vim.cmd, colorscheme_cmd)
+if not success then
+    vim.api.nvim_err_writeln(err)
+end
