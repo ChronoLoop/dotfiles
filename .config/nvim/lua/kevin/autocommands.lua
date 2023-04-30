86fe179010be8e4a021bdf90039be659c94a0bab
ChronoLoop <kevinwang1036@gmail.com>
2023-04-29 20:45:06 -0400

    updates

--- .config/nvim/lua/kevin/autocommands.lua
+++ .config/nvim/lua/kevin/autocommands.lua
@@ -1,11 +1,20 @@
-local function open_nvim_tree()
-    require('nvim-tree.api').tree.toggle({
-        focus = false,
-    })
+local function open_nvim_tree(data)
+    -- buffer is a real file on the disk
+    local real_file = vim.fn.filereadable(data.file) == 1
+
+    -- buffer is a [No Name]
+    local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
+
+    if not real_file and not no_name then
+        return
+    end
+
+    -- open the tree, find the file but don't focus it
+    require('nvim-tree.api').tree.toggle({ focus = false, find_file = true })
 end
 
-local function autocmd_callback()
-    open_nvim_tree()
+local function autocmd_callback(data)
+    open_nvim_tree(data)
     pcall(require('nvim-treesitter.install').update({ with_sync = true }))
 end
 
