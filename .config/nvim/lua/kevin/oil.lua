5d39433939246b0892db2d23571b70d1aeb0873f
ChronoLoop <kevinwang1036@gmail.com>
2023-06-03 18:29:26 -0400

    updates

--- .config/nvim/lua/kevin/oil.lua
+++ .config/nvim/lua/kevin/oil.lua
@@ -29,7 +29,7 @@ oil.setup({
         concealcursor = 'n',
     },
     -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`
-    default_file_explorer = true,
+    default_file_explorer = false,
     -- Restore window options to previous values when leaving an oil buffer
     restore_win_options = true,
     -- Skip the confirmation popup for simple operations
