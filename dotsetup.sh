782666bdb26a7a84899c69badff121668e0c19c8
Kevin Wang <kevinwang1036@gmail.com>
2024-07-17 11:38:08 -0400

    Update dotsetup.sh

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,3 +1,3 @@
-$ cd ~/dotfiles
-$ git config --local core.worktree $HOME
-$ git config --local status.showUntrackedFiles no
+cd ~/dotfiles
+git config --local core.worktree $HOME
+git config --local status.showUntrackedFiles no
