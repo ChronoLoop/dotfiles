23a6aa560fa03f3b4cb6cdb4605b6e91d0d3dda6
ChronoLoop <kevinwang1036@gmail.com>
2025-03-15 21:39:56 -0400

    updates

--- .config/nvim/lua/kevin/plugins/nvim-ufo.lua
+++ .config/nvim/lua/kevin/plugins/nvim-ufo.lua
@@ -2,6 +2,7 @@ return {
     'kevinhwang91/nvim-ufo',
     dependencies = { 'kevinhwang91/promise-async' },
     opts = {
+        open_fold_hl_timeout = 0,
         filetype_exclude = { 'help', 'nvim-tree', 'lazy', 'mason' },
         provider_selector = function(bufnr, filetype, buftype)
             return { 'lsp', 'indent' }
